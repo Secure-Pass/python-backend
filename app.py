fe36fbf019f4cefd0cc40070e7e7267301ae7ea9
Chidhambararajan <26043350+Chidhambararajan@users.noreply.github.com>
2021-05-27 19:43:16 +0530

    Added  support in app.py for heroku

--- app.py
+++ app.py
@@ -32,5 +32,5 @@ def hello():
     return "Website version Not yet developed"
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0",port=environ.get("PORT",5000))
+    app.run(host="0.0.0.0",port=os.environ.get("PORT",5000))
 
